67fbc546a490146ac23a25a0867d091dc5805825
Raheen <raheem.innovative@gmail.com>
2025-02-26 11:39:18 +0500

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,50 @@
+// The name of the cache
+const CACHE_NAME = 'calcuTeen-cache-v1';
+
+// Files to be cached
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/icon-192x192.png.png',
+  '/icon-512x512.png.png',
+  '/manifest.json',
+  '/sitemap.xml'
+];
+
+// Install event - Cache the necessary files
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('calcuteen-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/style.css',
-          '/script.js',
-          '/icons/icon-192x192.png',
-          '/icons/icon-512x512.png',
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching essential assets');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event - Serve cached assets or fetch from the network if not cached
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // Return the cached response if it exists, else fetch from the network
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
+// Activate event - Clear out old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
